ae8730eb74c155c67867614e4853f0cffd2b7e7f
franshu <fran.ojeda208@gmail.com>
2017-05-05 10:26:31 -0300

    Complete all squad

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,9 @@ function MiembroSquad(nombre,apellido,edad,hobbies){
 
 // hacer instancia de cada miembro con sus datos reales
 var cami = new MiembroSquad("Camila"," Saez",24,"Origami, leer");
+var cote = new MiembroSquad("María José"," Rodriguez",32,"Ver series, bailar");
 var anto = new MiembroSquad("Antonia"," Cerda",24,"Diseñar, leer");
 var caro = new MiembroSquad("Carolina"," Tapia",27,"Trekking, leer");
-var cote = new MiembroSquad("María José"," Rodriguez",32,"Ver series, bailar");
 var jose = new MiembroSquad("María José"," Pozo",31,"Bailar, leer");
 var val = new MiembroSquad("Valentina"," Saavedra",26,"Medicina autogestiva");
 var marion = new MiembroSquad("Marion"," Castillo",29,"Caminar, fotografia");
@@ -24,10 +24,44 @@ var fran= new MiembroSquad("Francisca"," Ojeda",28,"Roller Derby, lettering");
 //llamar al metodo
 //console.log(cami.mostrarSquad);
 
-var cotediv = document.getElementById("coteHTML");
-var camidiv = document.getElementById("camiHTML");
+
+var camiDiv = document.getElementById("camiHTML");
+var coteDiv = document.getElementById("coteHTML");
+var antoDiv = document.getElementById("antoHTML");
+var caroDiv = document.getElementById("caroHTML");
+var joseDiv = document.getElementById("joseHTML");
+var valDiv = document.getElementById("valHTML");
+var marionDiv = document.getElementById("marionHTML");
+var belenDiv = document.getElementById("belenHTML");
+var franDiv = document.getElementById("franHTML");
+
 var listaSquad ="<b>"+"Nombre: "+"</b>"+ cami.nombre + cami.apellido + "<br>" + "<b>"+"Edad: " +"</b>"+ cami.edad + "<br> " + "<b>"+ "Hobbies: " +"</b>"+ cami.hobbies+"<br>";
-camidiv.innerHTML = listaSquad; 
+camiDiv.innerHTML = listaSquad; 
 
 var listaSquad ="<b>"+"Nombre: "+"</b>"+ cote.nombre + cote.apellido + "<br>" + "<b>"+"Edad: " +"</b>"+ cote.edad + "<br> " + "<b>"+ "Hobbies: " +"</b>"+ cote.hobbies+"<br>";
-cotediv.innerHTML = listaSquad; 
+coteDiv.innerHTML = listaSquad; 
+
+var listaSquad ="<b>"+"Nombre: "+"</b>"+ anto.nombre + anto.apellido + "<br>" + "<b>"+"Edad: " +"</b>"+ anto.edad + "<br> " + "<b>"+ "Hobbies: " +"</b>"+ anto.hobbies+"<br>";
+antoDiv.innerHTML = listaSquad; 
+
+var listaSquad ="<b>"+"Nombre: "+"</b>"+ caro.nombre + caro.apellido + "<br>" + "<b>"+"Edad: " +"</b>"+ caro.edad + "<br> " + "<b>"+ "Hobbies: " +"</b>"+ caro.hobbies+"<br>";
+caroDiv.innerHTML = listaSquad;
+
+var listaSquad ="<b>"+"Nombre: "+"</b>"+ jose.nombre + jose.apellido + "<br>" + "<b>"+"Edad: " +"</b>"+ jose.edad + "<br> " + "<b>"+ "Hobbies: " +"</b>"+ jose.hobbies+"<br>";
+joseDiv.innerHTML = listaSquad;
+
+var listaSquad ="<b>"+"Nombre: "+"</b>"+ val.nombre + val.apellido + "<br>" + "<b>"+"Edad: " +"</b>"+ val.edad + "<br> " + "<b>"+ "Hobbies: " +"</b>"+ val.hobbies+"<br>";
+valDiv.innerHTML = listaSquad;
+
+var listaSquad ="<b>"+"Nombre: "+"</b>"+ marion.nombre + marion.apellido + "<br>" + "<b>"+"Edad: " +"</b>"+ marion.edad + "<br> " + "<b>"+ "Hobbies: " +"</b>"+ marion.hobbies+"<br>";
+marionDiv.innerHTML = listaSquad;
+
+var listaSquad ="<b>"+"Nombre: "+"</b>"+ belen.nombre + belen.apellido + "<br>" + "<b>"+"Edad: " +"</b>"+ belen.edad + "<br> " + "<b>"+ "Hobbies: " +"</b>"+ belen.hobbies+"<br>";
+belenDiv.innerHTML = listaSquad;
+
+var listaSquad ="<b>"+"Nombre: "+"</b>"+ fran.nombre + fran.apellido + "<br>" + "<b>"+"Edad: " +"</b>"+ fran.edad + "<br> " + "<b>"+ "Hobbies: " +"</b>"+ fran.hobbies+"<br>";
+franDiv.innerHTML = listaSquad;
+
+
+
+
