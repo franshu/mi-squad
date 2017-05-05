ca2e5bc345726ab670e2689f241813df5da114e5
franshu <fran.ojeda208@gmail.com>
2017-05-05 09:47:29 -0300

    now

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,24 +7,27 @@ function MiembroSquad(nombre,apellido,edad,hobbies){
 }
 
 // hacer instancia de cada miembro con sus datos reales
-var cami = new MiembroSquad("Camila","Saez",24,"Origami, leer");
-var anto = new MiembroSquad("Antonia","Cerda",24,"Diseñar, leer");
-var caro = new MiembroSquad("Carolina","Tapia",27,"Trekking, leer");
-var cote = new MiembroSquad("María José","Rodriguez",32,"Ver series, bailar");
-var jose = new MiembroSquad("María José","Pozo",31,"Bailar, leer");
-var val = new MiembroSquad("Valentina","Saavedra",26,"Medicina autogestiva");
-var marion = new MiembroSquad("Marion","Castillo",29,"Caminar, fotografia");
-var belen = new MiembroSquad("Belen","Recabal",29,"Andar en Bici, Netflix");
-var fran= new MiembroSquad("Francisca","Ojeda",28,"Roller Derby, lettering");
+var cami = new MiembroSquad("Camila"," Saez",24,"Origami, leer");
+var anto = new MiembroSquad("Antonia"," Cerda",24,"Diseñar, leer");
+var caro = new MiembroSquad("Carolina"," Tapia",27,"Trekking, leer");
+var cote = new MiembroSquad("María José"," Rodriguez",32,"Ver series, bailar");
+var jose = new MiembroSquad("María José"," Pozo",31,"Bailar, leer");
+var val = new MiembroSquad("Valentina"," Saavedra",26,"Medicina autogestiva");
+var marion = new MiembroSquad("Marion"," Castillo",29,"Caminar, fotografia");
+var belen = new MiembroSquad("Belen"," Recabal",29,"Andar en Bici, Netflix");
+var fran= new MiembroSquad("Francisca"," Ojeda",28,"Roller Derby, lettering");
 
 //metodos
 //this.mostrarSquad = function(){
 //	return(<h2>this.nombre + this.apellido <br> + this.edad <br>+this.hobbies)
-}
+//}
 //llamar al metodo
 //console.log(cami.mostrarSquad);
 
+var cotediv = document.getElementById("coteHTML");
+var camidiv = document.getElementById("camiHTML");
+var listaSquad ="<b>"+"Nombre: "+"</b>"+ cami.nombre + cami.apellido + "<br>" + "<b>"+"Edad: " +"</b>"+ cami.edad + "<br> " + "<b>"+ "Hobbies: " +"</b>"+ cami.hobbies+"<br>";
+camidiv.innerHTML = listaSquad; 
 
-var resultado = document.getElementById("lista1");
-var ListaSquad = ("Nombre: " + cami.nombre + cami.apellido + "<br> " + "Edad: " + cami.edad + "<br> " +  "Hobbies: " + cami.hobbies);
-resultado.innerHTML = ListaSquad; 
+var listaSquad ="<b>"+"Nombre: "+"</b>"+ cote.nombre + cote.apellido + "<br>" + "<b>"+"Edad: " +"</b>"+ cote.edad + "<br> " + "<b>"+ "Hobbies: " +"</b>"+ cote.hobbies+"<br>";
+cotediv.innerHTML = listaSquad; 
